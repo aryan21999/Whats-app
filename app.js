d5888fb9170ff64886f8b252c9b0dd0b2e36b757
aryan21999 <itsaryangupta1999@gmail.com>
2021-09-07 18:39:02 +0530

    addContact-index UI

--- app.js
+++ app.js
@@ -1,7 +1,40 @@
 const express = require('express')
+// const userRouter = require('./routers/user')
+const path = require('path')
 
 const app = express();
 const port = process.env.port || 3000
+
+
+app.use(express.static(__dirname + '././public'));
+const publicDirectoryPath = path.join(__dirname, '/views/')
+
+app.set("view engine", "ejs");
+
+app.get('/', function (req, res, next) {
+  res.render(`${publicDirectoryPath}signin`)
+})
+
+app.get('/index', function (req, res, next) {
+  res.render(`${publicDirectoryPath}index`)
+})
+
+app.get('/signup', function (req, res, next) {
+  res.render(`${publicDirectoryPath}signup`)
+})
+
+app.get('/addContact', function (req, res, next) {
+    res.render(`${publicDirectoryPath}addContact`)
+  })
+
+app.use(express.static(publicDirectoryPath))
+
+
+app.use(express.json())
+app.use(express.static("public"));
+
+// app.use(userRouter)
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}!`)
