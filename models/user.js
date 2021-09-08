3f784d1c0b49d41975d10c0c6fba4b41e50ac1bf
aryan21999 <itsaryangupta1999@gmail.com>
2021-09-08 14:02:41 +0530

    adding authentication

--- models/user.js
+++ models/user.js
@@ -30,8 +30,23 @@ const userSchema = new mongoose.Schema({
             }
         }
     },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
 
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismyfirstproject')
+
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
+}
 
 const User = mongoose.model('User', userSchema)
 
--- routers/user.js
+++ routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 router.post('/reg', async (req, res) => {
@@ -23,8 +24,30 @@ router.post('/reg/login', async (req, res) => {
     }
 })
 
-router.get('/reg/me', async (req, res) => {
+router.get('/reg/me', auth, async (req, res) => {
     res.send(req.user)
  })
+
+router.patch('/reg/me', auth, async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'password']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
+        console.log(user)
+
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+
 
 module.exports = router
