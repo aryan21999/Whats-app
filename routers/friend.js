4aaa0417f2814ea0b3ee13aa8210c69c009443db
aryan21999 <itsaryangupta1999@gmail.com>
2021-09-10 13:03:21 +0530

    list friends

--- routers/friend.js
+++ routers/friend.js
@@ -3,16 +3,52 @@ const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
 
-router.post('/friends', auth, async (req, res) => {
+router.post('/addFriends', auth, async (req, res) => {
   const user = await User.findOne({email:req.user.email})
   try {
-      await user.friends.push({name: req.body.name, email: req.body.email})
+      await user.friend.push({name: req.body.name, email: req.body.email})
       await user.save()
       res.status(200).send(user.friends[user.friends.length - 1])
+      console.log(user.friends)
+      console.log(user.friends.length)
   } catch (e) {
       res.status(400).send(e)
       console.log(e)
   }
 })
+router.get('/list', auth, async (req, res) => {
+  const user = await User.findOne({email:req.user.email})
+  try {
+      // const friend = await Friend.find({ owner: req.user.email })
+      res.send(user.friends)
+  } catch (e) {
+      res.status(500).send(e)
+      console.log(e)
+  }
+})
+
+router.get('/friends/:id', auth, async (req, res) => {
+  const _id = req.params.id
+
+  try {
+      const friend = await Friend.findOne({ _id, owner: req.user.email })
+
+      if (!friend) {
+          return res.status(404).send()
+      }
+      res.send(friend)
+  } catch (e) {
+      res.status(500).send()
+  }
+})
+
+// router.get('/list', auth, async (req,res) => {
+//   try{
+//       const allFriends = await Friends.find()
+//       res.status(200).send(allFriends)
+//   } catch (e) {
+//       res.status(500).send(e)
+//   }
+// })
 
 module.exports = router
